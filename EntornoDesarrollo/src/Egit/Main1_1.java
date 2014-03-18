075842ab014e1478a9f85ec4f2350bd3f4547d81
ZziLexX <ZziLexX@192.168.1.36>
2014-03-18 18:25:56 +0100

    This is the first VERSION!

---
+++ EntornoDesarrollo/src/Egit/Main1_1.java
@@ -0,0 +1,25 @@
+package Egit;
+
+import java.util.*;
+
+public class Main1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
+		resultado = suma(numero1, numero2);
+		System.out.println("Suma: " + resultado);
+	}
+
+	public static int suma(int a, int b) {
+		int c;
+		c = a + b;
+		return c;
+	}
+}
